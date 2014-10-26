682917db976d006f0d6c209a8d56eea604d41ce3
Marsfan <marsfan@users.noreply.github.com>
2014-10-26 17:21:43 -0500

    fixed issue #1, added comments

--- Control/Control.ino
+++ Control/Control.ino
@@ -1,27 +1,38 @@
+int pinUp = 2;
+int pinDown = 3;
+	
 void setup(){
+	//init pins for i/o shield
 	for(int i = 2; i < 8; i++){
 		pinMode(i, INPUT_PULLUP);
 	}
 	for(int i = 8; i < 14; i++){
 		pinMode(i, OUTPUT);
 	}
-	int pinUp = 4;
-	int pinDown = 3;
+	//set up serial monitor
 	Serial.begin(9600);
 }
 
 void loop(){
-	if(digitalRead(4) == HIGH){
+	//if switch is switched to up
+	if(digitalRead(pinUp) == LOW){
 		//print up
 		Serial.println("UP");
+		//light up led on pin 8
 		digitalWrite(8, HIGH);
 		}
-	else if(digitalRead(3) == HIGH){
+	//if switch is switched to down
+	else if(digitalRead(pinDown) == LOW){
 		//print down
 		Serial.println("DOWN");
+		//light up led on pin 9
 		digitalWrite(9, HIGH);
-		}else{
-			digitalWrite(9, LOW);
-			digitalWrite(8, LOW);
-		}
+	//otherwise
+	}else{
+		//turn off all LEDs and print off
+		digitalWrite(8, LOW);
+		digitalWrite(9, LOW);
+		Serial.println("OFF");
+}
+
 }
