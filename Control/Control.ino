4816a4bd17e1f6d7d5eb5c3462b46322f69fc067
Marsfan <marsfan@users.noreply.github.com>
2014-11-02 09:41:38 -0600

    added data for solenoid/motor shield control

--- Control/Control.ino
+++ Control/Control.ino
@@ -1,14 +1,26 @@
+//declare variables for toggle switch
 int pinUp = 2;
-int pinDown = 3;
+int pinDown = 4;
+//declare varaibles for motor shield
+int bleed = 12;
+int valve = 13;
+int bleedPower = 3;
+int valvePower = 11;
+//declare variables for valve states
+int valveOff = LOW;
+int bleedOn = HIGH;
+int valveOn = HIGH;
+int bleedOff = LOW;
 	
 void setup(){
-	//init pins for i/o shield
-	for(int i = 2; i < 8; i++){
-		pinMode(i, INPUT_PULLUP);
-	}
-	for(int i = 8; i < 14; i++){
-		pinMode(i, OUTPUT);
-	}
+	//init pins for toggle switch
+	pinMode(pinUp, INPUT_PULLUP);
+	pinMode(pinDown, INPUT_PULLUP):	
+	//set up motor shield
+	pinMode(bleed, OUTPUT);
+	pinMode(valve, OUTPUT);
+	pinMode(bleedPower, OUTPUT);
+	pinMode(valvePower, OUTPUT);
 	//set up serial monitor
 	Serial.begin(9600);
 }
@@ -18,21 +30,25 @@ void loop(){
 	if(digitalRead(pinUp) == LOW){
 		//print up
 		Serial.println("UP");
-		//light up led on pin 8
-		digitalWrite(8, HIGH);
+		//open valve
+		analogWrite(valvePower, 255);
+		digitalWrite(valve, valveOn);
+		digitalWrite(bleed, bleedOff);
 		}
 	//if switch is switched to down
 	else if(digitalRead(pinDown) == LOW){
 		//print down
 		Serial.println("DOWN");
-		//light up led on pin 9
-		digitalWrite(9, HIGH);
-	//otherwise
+		//open bleed valve
+		analogWrite(bleedPower, 255);
+		digitalWrite(bleed, bleedOn);
+		digitalWrite(valve, valveOff);
+	//if pin is in off position
 	}else{
-		//turn off all LEDs and print off
-		digitalWrite(8, LOW);
-		digitalWrite(9, LOW);
 		Serial.println("OFF");
+		digitalWrite(valve, valveOff);
+		digitalWrite(bleed, bleedOff);
+		
 }
 
 }
